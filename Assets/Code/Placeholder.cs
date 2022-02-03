e0487777fdfd38871c47cbbd05bdddb86fb9bcba
connlost <hi.imhpc@outlook.com>
2022-02-03 14:33:47 -0600

    setup

---
+++ Assets/Code/Placeholder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace placeholder
+{
+    public class Placeholder : MonoBehaviour
+    {
+        // Just an empty file making sure this folder can be tracked in git.
+    }
+}
+
---
+++ Assets/Textures/Placeholder.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeholder : MonoBehaviour
+{
+    // Just an empty file making sure this folder can be tracked in git.
+}
