898bad1015932fa8a4caa852452b9d0eec6baf05
connlost <hi.imhpc@outlook.com>
2022-02-18 00:13:00 -0600

    entity health calc

--- Assets/Scripts/Entity/JumpableEntity.cs
+++ Assets/Scripts/Entity/JumpableEntity.cs
@@ -1,28 +1,21 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace Code.Entity
-{
-    public class JumpableEntity: Entity
-    {
+namespace Entity {
+    public class JumpableEntity : Entity {
         public int maxJumps = 1;
         protected int CurrJumps;
 
-        protected new void Start()
-        {
+        protected new void Start() {
             base.Start();
             CurrJumps = maxJumps;
         }
-        private void OnCollisionStay2D(Collision2D other)
-        {
-            if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
-            {
+
+        private void OnCollisionStay2D(Collision2D other) {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Ground")) {
                 // to check we are indeed hitting ground other than top or side of Ground layer objects
                 RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, -transform.up, 0.7f);
-                foreach (RaycastHit2D hit in hits)
-                {
-                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
-                    {
+                foreach (RaycastHit2D hit in hits) {
+                    if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Ground")) {
                         CurrJumps = maxJumps;
                     }
                 }
