d0cc3a431772fc4a2f9935a79a49d1b892d6f5c9
connlost <hi.imhpc@outlook.com>
2022-02-18 00:06:36 -0600

    basic player item collection & consumption

--- Assets/Scripts/Entity/Player/PlayerEntity.cs
+++ Assets/Scripts/Entity/Player/PlayerEntity.cs
@@ -1,48 +1,77 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Item;
+using UnityEngine;
 
-namespace Code.Entity.Player
-{
-    public class PlayerEntity : JumpableEntity
-    {
+namespace Entity.Player {
+    public class PlayerEntity : JumpableEntity {
         public KeyCode keyUp = KeyCode.W;
         public KeyCode keyDown = KeyCode.S;
         public KeyCode keyLeft = KeyCode.A;
         public KeyCode keyRight = KeyCode.D;
         public KeyCode keyJump = KeyCode.Space;
+        public KeyCode keyInteraction = KeyCode.E;
+
+        private Dictionary<Items, int> _inventory;
         
-        private void Update()
-        {
-            if (Input.GetKey(keyUp))
-            {
+        private bool _isCrouching = false;
+
+        private new void Start() {
+            base.Start();
+            _inventory = new Dictionary<Items, int>();
+        }
+        
+        private void Update() {
+            if (Input.GetKey(keyUp)) {
                 // TODO climb?
             }
 
-            if (Input.GetKey(keyDown))
-            {
+            if (Input.GetKey(keyDown)) {
                 // TODO climb down?
                 // TODO crouch?
+                _isCrouching = true;
                 // TODO maybe some skills
             }
 
-            if (Input.GetKey(keyLeft))
-            {
+            if (Input.GetKey(keyLeft)) {
                 Rigidbody2D.AddForce(Vector2.left * 12f * Time.deltaTime, ForceMode2D.Impulse);
             }
 
-            if (Input.GetKey(keyRight))
-            {
+            if (Input.GetKey(keyRight)) {
                 Rigidbody2D.AddForce(Vector2.right * 12f * Time.deltaTime, ForceMode2D.Impulse);
             }
-            
+
             // jump
-            if (Input.GetKeyDown(keyJump))
-            {
-                if (CurrJumps > 0)
-                {
+            if (Input.GetKeyDown(keyJump)) {
+                if (CurrJumps > 0) {
                     --CurrJumps;
                     Rigidbody2D.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
                 }
             }
+            
+            Animator.SetBool("isCrouching", _isCrouching);
+        }
+
+        public Dictionary<Items, int> GetInventory() {
+            return _inventory;
+        }
+
+        public void OnItemCollect(Items item, int num) {
+            if (_inventory.ContainsKey(item)) {
+                _inventory[item] += num;
+            }
+            else {
+                _inventory.Add(item, num);
+            }
+        }
+
+        public int OnItemUsed(Items item, int num) {
+            if (_inventory.ContainsKey(item) && _inventory[item] >= num) {
+                _inventory[item] -= num;
+                return num;
+            }
+
+            return 0;
         }
     }
 }
