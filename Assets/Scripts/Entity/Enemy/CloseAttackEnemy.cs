25e665af930f152c84bc12412096d5bca4acf524
connlost <hi.imhpc@outlook.com>
2022-02-25 17:20:50 -0600

    make current enemies extends Entity

---
+++ Assets/Scripts/Entity/Enemy/CloseAttackEnemy.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entity.Enemy {
+    public class CloseAttackEnemy : Entity {
+        [SerializeField] private float moveSpeed;
+        private Transform _target;
+        public float attackDamage;
+        [SerializeField] private Transform targetA, targetB;
+        
+        // private SpriteRenderer _spriteRenderer;
+        
+        // [SerializeField] private bool isLeft;
+        protected new void Start() {
+            base.Start();
+            _target = targetB;
+            // _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+    
+        protected new void Update() {
+            base.Update();
+            Move();
+        }
+    
+        private void Move() {
+            if (Vector2.Distance(transform.position, targetA.position) <= 0.1f) {
+                _target = targetB;
+                // _spriteRenderer.flipX = false;
+                FlipFacing();
+                
+            }
+    
+            if (Vector2.Distance(transform.position, targetB.position) <= 0.1f) {
+                _target = targetA;
+                // _spriteRenderer.flipX = true;
+                FlipFacing();
+            }
+    
+            transform.position = Vector2.MoveTowards(transform.position, _target.position, moveSpeed * Time.deltaTime);
+        }
+    
+        private void OnCollisionStay2D(Collision2D collision) {
+            if (collision.collider.CompareTag("Player")) {
+                collision.gameObject.SendMessage("OnDamage", attackDamage);
+            }
+    
+            if (_target == targetA) {
+                _target = targetB;
+                // _spriteRenderer.flipX = false;
+                FlipFacing();
+            }
+            else {
+                _target = targetA;
+                // _spriteRenderer.flipX = true;
+                FlipFacing();
+            }
+        }
+    }
+}
