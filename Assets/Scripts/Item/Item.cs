a26b83127fee087439ac854398d99238c2e1e080
connlost <hi.imhpc@outlook.com>
2022-02-25 19:38:13 -0600

    placeholders for items

---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Item {
+    public class Item : MonoBehaviour{
+        // common behaviors for item objects
+        protected void Start() {
+            throw new NotImplementedException();
+        }
+
+        protected void Update() {
+            throw new NotImplementedException();
+        }
+    }
+}
