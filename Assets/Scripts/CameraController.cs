0b16e56caae263025887bfbbcdcdc0d595423978
connlost <hi.imhpc@outlook.com>
2022-04-08 03:14:58 -0500

    Update CinemachineConfiner2D on map loaded

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using Cinemachine;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+    public static CameraController Instance;
+    private CinemachineConfiner2D _confiner;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Start() {
+        _confiner = gameObject.GetComponent<CinemachineConfiner2D>();
+    }
+
+    public void UpdateConfiner(Collider2D boundingShape) {
+        if (_confiner) {
+            _confiner.m_BoundingShape2D = boundingShape;
+        }
+
+    }
     
 }
