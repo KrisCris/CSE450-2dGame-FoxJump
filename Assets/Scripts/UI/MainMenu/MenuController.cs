aaa471ae4ac1c202624a2db60bd6cd60182dafa5
connlost <hi.imhpc@outlook.com>
2022-04-08 03:12:45 -0500

    Add MenuController for MainMenu in supporting more features

---
+++ Assets/Scripts/UI/MainMenu/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace UI.MainMenu {
+    public class MenuController: MonoBehaviour {
+        public GameObject newGameButton;
+        public GameObject continueButton;
+        public GameObject selectLevelButton;
+
+        private void Start() {
+            if (GameController.Instance.selectableLevel.Count == 0) {
+                continueButton.SetActive(false);
+                selectLevelButton.SetActive(false);
+            }
+
+            Debug.Log(PlayerPrefs.GetString("GameData"));
+        }
+
+        public void NewGame() {
+            SceneController.Instance.LoadGame("Scene_0");
+        }
+
+        public void ContinueGame() {
+            SceneController.Instance.LoadGame(GameController.Instance.currentLevel);
+        }
+
+        public void SelectLevel() {
+            foreach (var level in GameController.Instance.selectableLevel) {
+                Debug.Log("Selectable: " + level);
+            }
+        }
+    }
+}
--- Assets/Scripts/UI/RestartButton.cs
+++ Assets/Scripts/UI/RestartButton.cs
@@ -1,25 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using Entity.Player;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
-public class RestartButton : MonoBehaviour
-{
-
-    // public Button button;
-    void Start()
-    {
-        GetComponent<Button>().onClick.AddListener(() =>
-        {
-            SceneManager.UnloadSceneAsync("Scene_0");
-            SceneManager.UnloadSceneAsync("Player");
-            SceneManager.UnloadSceneAsync("info");
-            // SceneManager.LoadScene("GamePlay");
-            SceneManager.LoadScene("Scene_0", LoadSceneMode.Additive);
-            SceneManager.LoadScene("Player", LoadSceneMode.Additive);
-            Time.timeScale = 1;
-        });
+namespace UI {
+    public class RestartButton : MonoBehaviour {
+        // public Button button;
+        void Start() {
+            // GetComponent<Button>().onClick.AddListener(() =>
+            // {
+            //     SceneManager.UnloadSceneAsync("Scene_0");
+            //     SceneManager.UnloadSceneAsync("Player");
+            //     SceneManager.UnloadSceneAsync("Info");
+            //     // SceneManager.LoadScene("GamePlay");
+            //     SceneManager.LoadScene("Scene_0", LoadSceneMode.Additive);
+            //     SceneManager.LoadScene("Player", LoadSceneMode.Additive);
+            //     Time.timeScale = 1;
+            // });
+            if (SceneController.Instance) {
+                GetComponent<Button>().onClick.AddListener(SceneController.Instance.ReloadCurrentScene);
+            }
+        }
     }
 }
