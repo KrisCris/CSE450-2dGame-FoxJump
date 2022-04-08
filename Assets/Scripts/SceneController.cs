d8a7bb413a0ea8f22ffaa2d9b2f3fcbac665afe1
connlost <hi.imhpc@outlook.com>
2022-04-08 03:10:02 -0500

    Update scene switch mechanism

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,19 +5,43 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
-    public static SceneController instance;
+    public static SceneController Instance;
+    
+    private string[] _gameSceneNames = new[] {
+        "Scene_0"
+    };
+    
     private void Awake() {
-        instance = this;
+        Instance = this;
     }
-    void Start()
+    private void Start()
     {
-        // SceneManager.LoadScene("UI", LoadSceneMode.Additive);
-        // SceneManager.LoadScene("Test_2_hpc", LoadSceneMode.Additive);
         SceneManager.LoadScene("MainUI", LoadSceneMode.Additive);
     }
 
-    public void SwitchScene(String scene) {
+    public void LoadGame(string scene) {
+        SceneManager.UnloadSceneAsync("MainUI");
+        SceneManager.LoadScene("Player", LoadSceneMode.Additive);
+        SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+        GameController.Instance.SetCurrentScene(scene);
+    }
+
+    public void SwitchMap(String scene) {
         SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
         SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+        GameController.Instance.SetLevelClear(SceneManager.GetActiveScene().name);
+        GameController.Instance.SetCurrentScene(scene);
+    }
+
+    public void ReloadCurrentScene() {
+        UnloadAll();
+        SceneManager.LoadScene("Player", LoadSceneMode.Additive);
+        SceneManager.LoadScene(PlayerPrefs.GetString("CurrentMap"), LoadSceneMode.Additive);
+    }
+
+    public void UnloadAll() {
+        for (int i = 1; i < SceneManager.sceneCount; i++) {
+            SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i));
+        }
     }
 }
