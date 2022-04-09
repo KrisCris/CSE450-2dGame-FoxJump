8f26223ddbb2d222f5fc6fba34d6da4ddf50dfc9
connlost <hi.imhpc@outlook.com>
2022-04-08 21:02:39 -0500

    fix merge

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -4,27 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class SoundManager : MonoBehaviour {
-    public static SoundManager instance;
+public class SoundController : MonoBehaviour {
+    public static SoundController Instance;
 
     private AudioSource _audioSource;
 
     private void Awake() {
-        instance = this;
+        Instance = this;
     }
 
     // Start is called before the first frame update
     void Start() {
         _audioSource = GetComponent<AudioSource>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void playSound(AudioClip sound) {
+    
+    public void PlaySound(AudioClip sound) {
         _audioSource.PlayOneShot(sound);
     }
 }
