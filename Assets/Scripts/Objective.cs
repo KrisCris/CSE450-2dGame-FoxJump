edbdf205ffcb225e96095c1cd905f764a8bba015
connlost <hi.imhpc@outlook.com>
2022-04-09 00:02:07 -0500

    a simple indoor scene

--- Assets/Scripts/Objective.cs
+++ Assets/Scripts/Objective.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Objective : MonoBehaviour {
     public string sceneName;
+    public bool requireKeyInteraction = false;
     private Collider2D _collider2D;
 
     private void Start() {
@@ -14,7 +15,7 @@ public class Objective : MonoBehaviour {
     }
 
     private void OnTriggerStay2D(Collider2D other) {
-        if (Input.GetKey(KeyCode.E)) {
+        if (!requireKeyInteraction || Input.GetKey(KeyCode.E)) {
             if (other.GetComponent<PlayerEntity>()) {
                 SceneController.Instance.SwitchMap(sceneName);
             }
