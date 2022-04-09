a0151a9deb2ba13e620f78ae4a7fd3ebca159aca
connlost <hi.imhpc@outlook.com>
2022-04-08 19:00:09 -0500

    Init for universal KeyBinds control

---
+++ Assets/Scripts/KeyBinds.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class KeyBinds: MonoBehaviour {
+    public static KeyBinds Instance;
+
+    public KeyCode climb = KeyCode.W;
+    public KeyCode jump = KeyCode.Space;
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveRight = KeyCode.D;
+    public KeyCode interaction = KeyCode.E;
+    public KeyCode attack = KeyCode.Mouse0;
+    
+    private void Awake() {
+        Instance = this;
+    }
+}
