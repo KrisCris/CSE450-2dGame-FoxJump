bf980e145cd4025a10babe55a86782f44b317639
connlost <hi.imhpc@outlook.com>
2022-04-08 22:33:12 -0500

    bg movement, but broken

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+    public Transform backgroundImagePos;
+    
+    private void Start() {
+        if (!CameraController.Instance) {
+            Debug.Log("BackgroundController::Start >>> CameraController hasn't been initialized!");
+        }
+    }
+
+    private void Update() {
+        if (!backgroundImagePos) {
+            Debug.Log("BackgroundController::Update >>> background not found!");
+            return;
+        }
+        backgroundImagePos.position = CameraController.Instance.GetCamPos2D();
+    }
+}
