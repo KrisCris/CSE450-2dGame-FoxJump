5b8bd4db06797b6066c02c67dade440e1833ed30
xingchen yang <xingchen@wustl.edu>
2022-04-14 22:34:47 -0500

    Fix collider between items and mobs

--- Assets/Scripts/Entity/Enemy/HeadKill.cs
+++ Assets/Scripts/Entity/Enemy/HeadKill.cs
@@ -3,8 +3,18 @@ using UnityEngine;
 namespace Entity.Enemy {
     public class HeadKill : MonoBehaviour {
         public GameObject mob;
+        public AudioClip hitsound;
+        public AudioSource audioSource;
+
+
+        private new void Start()
+        {
+            
+            audioSource = GetComponent<AudioSource>();
+        }
         private void OnTriggerEnter2D(Collider2D other) {
             if (other.CompareTag("Player")) {
+                SoundController.Instance.PlaySound(hitsound);
                 Destroy(mob);
             }
         }
