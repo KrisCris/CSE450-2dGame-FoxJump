2d118e9ea85b59e11bc4309b95df14491983a7ff
connlost <hi.imhpc@outlook.com>
2022-04-17 21:04:33 -0500

    Fox Tail item

--- Assets/Scripts/Item/FoxTail.cs
+++ Assets/Scripts/Item/FoxTail.cs
@@ -1,5 +1,13 @@
-﻿namespace Item {
+﻿using Entity.Player;
+using UI;
+
+namespace Item {
     public class FoxTail : Item {
-        
+        protected override void OnItemCollect(PlayerEntity player) {
+            if (MessageController.Instance && GameController.Instance && !GameController.Instance.hasFoxTail) {
+                MessageController.Instance.ShowMessage("A tail from the fox that is good at climb walls in the past...");
+            }
+            base.OnItemCollect(player);
+        }
     }
 }
