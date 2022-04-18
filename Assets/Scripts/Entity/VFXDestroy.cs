686a99d59a8e26c5df8974ebc74b599007bcefe4
connlost <hi.imhpc@outlook.com>
2022-04-18 06:37:23 -0500

    auto destroy vfxs

---
+++ Assets/Scripts/Entity/VFXDestroy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Entity {
+    public class VFXDestroy : MonoBehaviour{
+        private void Start() {
+            Destroy(gameObject, 1);
+        }
+    }
+}
