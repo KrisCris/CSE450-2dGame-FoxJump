0eac65ef953d1b858608a01f9fc5ea2cc3f8db4b
connlost <hi.imhpc@outlook.com>
2022-04-18 00:28:40 -0500

    message model

---
+++ Assets/Scripts/UI/Messages.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI {
+    public class Messages {
+        public List<string> msgs;
+        public int idx;
+
+        public Messages(List<string> msgs) {
+            this.msgs = msgs;
+            idx = -1;
+        }
+
+        public bool HasNext() {
+            return idx < msgs.Count-1;
+        }
+
+        public string Next() {
+            if (HasNext()) {
+                return msgs[++idx];
+            }
+
+            return msgs[^1];
+        }
+
+        public void RevertLast() {
+            idx = Mathf.Max(-1, idx - 1);
+        }
+    }
+}
