aac067935a33a14c18bb0cd52df58286a5fecd8f
connlost <hi.imhpc@outlook.com>
2022-04-18 01:56:42 -0500

    layered grass

---
+++ Assets/Scripts/Ground/Grass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Ground {
+    public class Grass : MonoBehaviour {
+        private Animator _animator;
+        
+        private void Start() {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D col) {
+            _animator.SetTrigger("Touching");
+        }
+    }
+}
