1edda73e33c8e31603c73cb30d015117cd9f479a
connlost <hi.imhpc@outlook.com>
2022-04-19 17:41:12 -0500

    Modifications

---
+++ Assets/Scripts/Ground/FallingSpikes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ground {
+    public class FallingSpikes : MonoBehaviour, ITriggerable {
+        private bool _triggered = false;
+        public AudioSource fallingAudio;
+        public void Trigger() {
+            if (!_triggered) {
+                if (!gameObject.GetComponent<Rigidbody2D>()) {
+                    var rb = gameObject.AddComponent<Rigidbody2D>();
+                    rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                    rb.mass = 100;
+                    if (fallingAudio && !fallingAudio.isPlaying) {
+                        fallingAudio.Play();
+                    }
+                    _triggered = true;
+                }
+            }
+
+        }
+
+        public bool HasTriggered() {
+            return _triggered;
+        }
+
+        private void OnBecameInvisible() {
+            if (_triggered) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Ground/ItemTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ground {
+    public class ItemTrigger : MonoBehaviour {
+        public List<GameObject> triggerableItems;
+
+        private void OnTriggerEnter2D(Collider2D col) {
+            foreach (GameObject triggerableItem in triggerableItems) {
+                if (!triggerableItem.GetComponent<ITriggerable>().HasTriggered()) {
+                    triggerableItem.GetComponent<ITriggerable>().Trigger();
+                }
+            }
+        }
+    }
+}
