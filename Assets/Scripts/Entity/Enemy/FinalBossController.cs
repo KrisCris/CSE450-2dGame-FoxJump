caafb59323acc0d5de4f06d6ae4dcf4bfe3987b5
connlost <hi.imhpc@outlook.com>
2022-04-20 00:56:43 -0500

    ladder falls on defeat final boss

--- Assets/Scripts/Entity/Enemy/FinalBossController.cs
+++ Assets/Scripts/Entity/Enemy/FinalBossController.cs
@@ -9,10 +9,15 @@ namespace Entity.Enemy {
         public List<GameObject> triggerableItems;
         public Boss boss;
         public Collider2D cameraBound;
-        public Collider2D arenaBound;
+        public Collider2D arenaBoundL;
+        public Collider2D arenaBoundR;
         public AudioSource prevAudio;
         public AudioSource bossAudio;
         public string endGameScene;
+
+        public GameObject ladder;
+        public AudioSource ladderSound;
+        
         private PlayerEntity _player;
         private bool _triggered;
         private bool _attacking;
@@ -22,14 +27,19 @@ namespace Entity.Enemy {
             _triggered = false;
             _attacking = false;
             _shownDieMessage = false;
+            ladder.SetActive(false);
         }
 
         private void OnTriggerEnter2D(Collider2D other) {
             if (!_triggered && other.gameObject.GetComponent<PlayerEntity>()) {
                 Debug.Log("try trigger boss fight");
                 // activate arena bound
-                if (arenaBound) {
-                    arenaBound.enabled = true;
+                if (arenaBoundL) {
+                    arenaBoundL.enabled = true;
+                }
+
+                if (arenaBoundR) {
+                    arenaBoundR.enabled = true;
                 }
 
                 // Init Boss
@@ -69,10 +79,30 @@ namespace Entity.Enemy {
             }
 
             if (boss.GetComponent<Boss>().IsDead() && !_shownDieMessage) {
+                arenaBoundR.enabled = false;
+                // bossAudio.Stop();
                 if (SceneController.Instance) {
                     if (MessageController.Instance) {
                         MessageController.Instance.ShowMessage("Boss defeated...\nTime to go home...",
-                            () => { SceneController.Instance.SwitchMap(endGameScene); });
+                            () => {
+                                if (ladder) {
+                                    // Enable Ladder
+                                    ladder.SetActive(true);
+                                    // Avoid Player Collision
+                                    Physics2D.IgnoreCollision(_player.gameObject.GetComponent<CapsuleCollider2D>(),
+                                        ladder.GetComponent<BoxCollider2D>());
+                                    // Let it naturally fall down to the ground
+                                    var rb = ladder.AddComponent<Rigidbody2D>();
+                                    rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                                    rb.mass = 1000;
+                                    // Try play a sound
+                                    if (ladderSound && !ladderSound.isPlaying) {
+                                        ladderSound.Play();
+                                    }
+                                } else {
+                                    SceneController.Instance.SwitchMap(endGameScene);
+                                }
+                            });
                     } else {
                         SceneController.Instance.SwitchMap(endGameScene);
                     }
