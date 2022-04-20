0d18977acc63466502e08e19d954b516c8f66fd0
connlost <hi.imhpc@outlook.com>
2022-04-19 20:04:49 -0500

    falling spikes

--- Assets/Scripts/Ground/ItemTrigger.cs
+++ Assets/Scripts/Ground/ItemTrigger.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using Entity.Player;
 using UnityEngine;
 
 namespace Ground {
     public class ItemTrigger : MonoBehaviour {
         public List<GameObject> triggerableItems;
+        private bool _triggered;
+
+        private void Awake() {
+            _triggered = false;
+        }
 
         private void OnTriggerEnter2D(Collider2D col) {
-            foreach (GameObject triggerableItem in triggerableItems) {
-                if (!triggerableItem.GetComponent<ITriggerable>().HasTriggered()) {
-                    triggerableItem.GetComponent<ITriggerable>().Trigger();
+            if (col.gameObject.GetComponent<PlayerEntity>() && !_triggered) {
+                foreach (GameObject triggerableItem in triggerableItems) {
+                    if (!triggerableItem.GetComponent<ITriggerable>().HasTriggered()) {
+                        triggerableItem.GetComponent<ITriggerable>().Trigger();
+                    }
                 }
+
+                _triggered = true;
             }
+
         }
     }
 }
